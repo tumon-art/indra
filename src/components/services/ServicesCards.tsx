ca0792179e87b60b05a81bf6c49fb665a7af07f9
tumon <tumonkhn@gmail.com>
2022-10-15 19:50:04 +0600

    init

--- src/components/services/ServicesCards.tsx
+++ src/components/services/ServicesCards.tsx
@@ -1,8 +1,14 @@
 interface Props {
-  svg: SVGAElement;
+  svg: SVGAElement | any;
   text: string;
 }
 export default function ServicesCards({ svg, text }: Props) {
   console.log(svg);
-  return <div></div>;
+  return (
+    <div className="">
+      <div className=" text-primary w-20 h-20">{svg}</div>
+
+      <p> {text} </p>
+    </div>
+  );
 }
